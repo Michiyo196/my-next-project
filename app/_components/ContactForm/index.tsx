0566e7c7311ec50391938e543197fa152412d22b
Michiyo196 <info@route196.com>
2025-04-28 16:17:28 +0900

    Contact form1

---
+++ app/_components/ContactForm/index.tsx
@@ -0,0 +1,64 @@
+import styles from "./index.module.css";
+
+export default function ContactForm() {
+  return (
+    <form className={styles.form}>
+      <div className={styles.horizontal}>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="lastname">
+            姓
+          </label>
+          <input
+            className={styles.textfield}
+            type="text"
+            id="lastname"
+            name="lastname"
+          />
+        </div>
+
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="firstname">
+            名
+          </label>
+          <input
+            className={styles.textfield}
+            type="text"
+            id="firstname"
+            name="firstname"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <label className={styles.label} htmlFor="company">
+          会社名
+        </label>
+        <input
+          className={styles.textfield}
+          type="text"
+          id="company"
+          name="company"
+        />
+      </div>
+      <div className={styles.item}>
+        <label className={styles.label} htmlFor="email">
+          メールアドレス
+        </label>
+        <input
+          className={styles.textfield}
+          type="text"
+          id="email"
+          name="email"
+        />
+      </div>
+      <div className={styles.item}>
+        <label className={styles.label} htmlFor="message">
+          メールアドレス
+        </label>
+        <textarea className={styles.textarea} id="message" name="message" />
+      </div>
+      <div className={styles.actions}>
+        <input type="submit" value="送信する" className={styles.button}></input>
+      </div>
+    </form>
+  );
+}
